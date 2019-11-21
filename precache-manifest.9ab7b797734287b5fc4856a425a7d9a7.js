8e37c9610d1911166cf2b4d22e2e0777ed206193
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2019-11-21 12:07:23 +0700

    Updates

---
+++ precache-manifest.9ab7b797734287b5fc4856a425a7d9a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9257d818f2c42f2ff208",
+    "url": "/note-json-server/static/js/runtime~main.efa16f9b.js"
+  },
+  {
+    "revision": "01f3498e13399ee4cdce",
+    "url": "/note-json-server/static/js/main.979681b4.chunk.js"
+  },
+  {
+    "revision": "ae8fdf481d2594bf6fad",
+    "url": "/note-json-server/static/js/2.7d8e9f29.chunk.js"
+  },
+  {
+    "revision": "01f3498e13399ee4cdce",
+    "url": "/note-json-server/static/css/main.96731820.chunk.css"
+  },
+  {
+    "revision": "cc73952310ebbca038411d4ae0541c39",
+    "url": "/note-json-server/index.html"
+  }
+];
